2da57d2dfe74e54626cbc31a51d43dfb5b9e23fd
syedfaizz <syedfaizz45@gmail.com>
2021-09-15 13:18:47 +0530

    [faisal] Add: uc3

--- Employee.js
+++ Employee.js
@@ -4,17 +4,19 @@ const PART_TIME_HOURS =4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOURS=20;
 let empHrs=0;
-let empCheck=Math.floor(Math.random()*10)%3;
-switch(empCheck)
+function getWorkingHours(empCheck)
 {
-    case IS_PART_TIME:
-        empHrs=PART_TIME_HOURS;
-        break;
-    case IS_fULL_tIME:
-        empHrs=FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs=0;
+    switch(empCheck)
+    {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_fULL_tIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
+let empCheck=Math.floor(Math.random()*10)%3;
+empHrs=getWorkingHours(empCheck);
 let empWage= empHrs*WAGE_PER_HOURS;
 console.log("Employee Wage: "+ empWage);
