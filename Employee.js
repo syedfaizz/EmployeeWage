e97f8cf698b7cc073b49794f29d8e7038ab676b0
syedfaizz <syedfaizz45@gmail.com>
2021-09-15 13:24:17 +0530

    [faisal] Add: uc4

--- Employee.js
+++ Employee.js
@@ -3,6 +3,7 @@ const IS_fULL_tIME =2;
 const PART_TIME_HOURS =4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOURS=20;
+const WORKING_DAYS=20;
 let empHrs=0;
 function getWorkingHours(empCheck)
 {
@@ -16,7 +17,10 @@ function getWorkingHours(empCheck)
             return 0;
     }
 }
-let empCheck=Math.floor(Math.random()*10)%3;
-empHrs=getWorkingHours(empCheck);
+for(let day=0;day<WORKING_DAYS;day++)
+{
+    let empCheck = Math.floor(Math.random()*10)%3;
+    empHrs += getWorkingHours(empCheck);
+}
 let empWage= empHrs*WAGE_PER_HOURS;
 console.log("Employee Wage: "+ empWage);
